0c3beef032bb408faf891dd7a98cf8c4c91b7cdd
cuiyang <cuiyangshou@qq.com>
2018-03-12 22:36:19 +0800

    检测设备状态

--- src/main/java/org/cuiyang/projection/Start.java
+++ src/main/java/org/cuiyang/projection/Start.java
@@ -20,20 +20,13 @@ public class Start extends Application {
 
     public static Stage primaryStage;
 
-    /** 设备管理器 */
-    private DeviceManager deviceManager;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         Start.primaryStage = primaryStage;
-        deviceManager = new DeviceManager();
-        deviceManager.init();
 
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getResource("main.fxml"));
         Parent root = fxmlLoader.load();
-        MainController controller = fxmlLoader.getController();
-        controller.setDeviceManager(deviceManager);
 
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("投屏1.0");
--- src/main/java/org/cuiyang/projection/controller/MainController.java
+++ src/main/java/org/cuiyang/projection/controller/MainController.java
@@ -1,12 +1,14 @@
 package org.cuiyang.projection.controller;
 
 import com.android.ddmlib.IDevice;
+import javafx.application.Platform;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Paint;
 import org.cuiyang.projection.Start;
 import org.cuiyang.projection.core.DeviceManager;
 import org.cuiyang.projection.core.ScreenListener;
@@ -38,15 +40,23 @@ public class MainController implements ScreenListener, Initializable{
     }
 
     public void start() throws Exception {
-        IDevice[] devices = deviceManager.getDevices();
-        if (devices == null || devices.length == 0) {
-            Alert alert = new Alert(Start.primaryStage);
-            alert.showAndWait();
-            return;
+        if (start.getText().equals("启动")) {
+            // 启动
+            if (device != null && device.isOnline()) {
+                screenProjection = new ScreenProjection(device, this);
+                screenProjection.start();
+                start.setText("暂停");
+            } else {
+                Alert alert = new Alert(Start.primaryStage);
+                alert.showAndWait();
+            }
+        } else {
+            // 暂停
+            if (screenProjection != null) {
+                screenProjection.close();
+            }
+            start.setText("启动");
         }
-        device = devices[0];
-        screenProjection = new ScreenProjection(device, this);
-        screenProjection.start();
     }
 
     @Override
@@ -59,9 +69,36 @@ public class MainController implements ScreenListener, Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        deviceManager = new DeviceManager();
+        deviceManager.init();
+
+        new Thread(() -> {
+            while (true) {
+                IDevice[] devices = deviceManager.getDevices();
+                if (devices != null && devices.length > 0) {
+                    device = devices[0];
+                    Platform.runLater(() -> {
+                        bottomInfo.setText(device.getName() + " - " + device.getState().name().toLowerCase());
+                        bottomInfo.setTextFill(Paint.valueOf("green"));
+                    });
+                } else {
+                    // 关闭同屏
+                    if (screenProjection != null) {
+                        screenProjection.close();
+                    }
+                    start.setText("启动");
+                    Platform.runLater(() -> {
+                        bottomInfo.setText("未连接");
+                        bottomInfo.setTextFill(Paint.valueOf("red"));
+                    });
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
-    public void setDeviceManager(DeviceManager deviceManager) {
-        this.deviceManager = deviceManager;
-    }
 }
