eb16d820c0ebc79b85cbde32429c012d6eccf1d8
cuiyang <cuiyangshou@qq.com>
2018-03-19 22:52:17 +0800

    添加缩放功能

--- src/main/java/org/cuiyang/projection/controller/MainController.java
+++ src/main/java/org/cuiyang/projection/controller/MainController.java
@@ -8,13 +8,15 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Paint;
+import org.cuiyang.minicap.ScreenListener;
+import org.cuiyang.minicap.ScreenProjection;
 import org.cuiyang.projection.Start;
 import org.cuiyang.projection.core.DeviceManager;
-import org.cuiyang.projection.core.ScreenListener;
-import org.cuiyang.projection.core.ScreenProjection;
 import org.cuiyang.projection.widget.Alert;
 
+import java.io.ByteArrayInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -36,14 +38,18 @@ public class MainController implements ScreenListener, Initializable{
     private ScreenProjection screenProjection;
     private DeviceManager deviceManager;
 
+    private float scale = 0.4f;
+
     public MainController() {
     }
 
     public void start() throws Exception {
-        if (start.getText().equals("启动")) {
+        if ("启动".equals(start.getText())) {
             // 启动
             if (device != null && device.isOnline()) {
                 screenProjection = new ScreenProjection(device, this);
+                screenProjection.setZoom(this.scale);
+                screenProjection.setQuality(80);
                 screenProjection.start();
                 start.setText("暂停");
             } else {
@@ -60,19 +66,54 @@ public class MainController implements ScreenListener, Initializable{
     }
 
     @Override
-    public void projection(Image frame) {
-        canvas.setWidth(frame.getWidth());
-        canvas.setHeight(frame.getHeight());
+    public void projection(byte[] frame) {
+        Image image = new Image(new ByteArrayInputStream(frame));
+        canvas.setWidth(image.getWidth());
+        canvas.setHeight(image.getHeight());
         GraphicsContext graphics = canvas.getGraphicsContext2D();
-        graphics.drawImage(frame, 0, 0);
+        graphics.drawImage(image, 0, 0);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         deviceManager = new DeviceManager();
         deviceManager.init();
+        new DeviceMonitor().start();
+    }
 
-        new Thread(() -> {
+    /**
+     * 缩小
+     * @param mouseEvent MouseEvent
+     */
+    public void zoomDown(MouseEvent mouseEvent) {
+        this.scale -= 0.1;
+        update();
+    }
+
+    /**
+     * 放大
+     * @param mouseEvent MouseEvent
+     */
+    public void zoomUp(MouseEvent mouseEvent) {
+        this.scale += 0.1;
+        update();
+    }
+
+    /**
+     * 更新配置
+     */
+    private void update() {
+        this.screenProjection.setZoom(this.scale);
+        this.screenProjection.restart();
+    }
+
+    /**
+     * 设备监控
+     */
+    private class DeviceMonitor extends Thread {
+        @Override
+        public void run() {
+            // noinspection InfiniteLoopStatement
             while (true) {
                 IDevice[] devices = deviceManager.getDevices();
                 if (devices != null && devices.length > 0) {
@@ -82,23 +123,23 @@ public class MainController implements ScreenListener, Initializable{
                         bottomInfo.setTextFill(Paint.valueOf("green"));
                     });
                 } else {
+                    device = null;
                     // 关闭同屏
                     if (screenProjection != null) {
                         screenProjection.close();
                     }
-                    start.setText("启动");
                     Platform.runLater(() -> {
+                        start.setText("启动");
                         bottomInfo.setText("未连接");
                         bottomInfo.setTextFill(Paint.valueOf("red"));
                     });
                 }
                 try {
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                } catch (InterruptedException ignore) {
                 }
             }
-        }).start();
+        }
     }
 
 }
